2545dbeeaa44a19bfc348c1d37ba95c0d23864bd
mandara11 <mandaram2345@gmail.com>
2022-08-16 20:06:11 +0530

    Added:UC2

--- Program.cs
+++ Program.cs
@@ -11,6 +11,12 @@
             Console.WriteLine("Please Enter your first name");
             var check = firstName.ValidateFirstName(Console.ReadLine());
             Console.WriteLine(check);
+
+            //UC2
+            LastName lastName = new LastName();
+            Console.WriteLine("Please Enter your last name");
+            var check2 = lastName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check2);
         }
     }
 }
