7144e6db88cd44f29a9d0b24b401afa684b662d8
mandara11 <mandaram2345@gmail.com>
2022-08-16 22:28:38 +0530

    AtleastOneUpperCase

---
+++ Password2.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+/*
+Rule2 – Should have at least 1 Upper Case
+*/
+
+namespace UserRegistrationProblem
+{
+    public class Password2
+    {
+        public static string REGEX_Password2 = "^[A-Z]{1}[A-Z a-z]{7,}$"; //Mandara
+
+        public bool ValidatePassword2(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password2);
+        }
+    }
+}
