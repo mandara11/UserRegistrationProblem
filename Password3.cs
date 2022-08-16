b30f32b29d2e7ef71c1ccbac4d1809b2e55863c7
mandara11 <mandaram2345@gmail.com>
2022-08-16 22:34:03 +0530

    AtleastOneNumericNumber

---
+++ Password3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+/*
+Rule3 – Should have at least 1 numeric number in the password
+- NOTE – All rules must be passed
+*/
+
+namespace UserRegistrationProblem
+{
+    public class Password3
+    {
+        public static string REGEX_Password3 = "^[A-Z]{1}[A-Z a-z]{7,}[0-9]{1,}$";  //Mandara23
+
+        public bool ValidatePassword3(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password3);
+        }
+    }
+}
