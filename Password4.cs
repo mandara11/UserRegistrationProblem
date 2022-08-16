6cb4e82f7e2d22bc068611657b1545f5b2c680fa
mandara11 <mandaram2345@gmail.com>
2022-08-16 22:39:24 +0530

    ExactlyOneSpecialCharacter

---
+++ Password4.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+/*
+Rule4 – Has exactly 1 Special Character
+- NOTE – All rules must be passed
+*/
+
+namespace UserRegistrationProblem
+{
+    public class Password4
+    {
+        public static string REGEX_Password4 = "^[A-Z]{1}[A-Z a-z]{7,}[!*@#$%^&+=]?[0-9]{1,}$"; //Mandaram@1234
+
+        public bool ValidatePassword4(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password4);
+        }
+    }
+}
