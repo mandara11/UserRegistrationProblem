d11b885474cb0998881485b38eba0aa65a58ceca
mandara11 <mandaram2345@gmail.com>
2022-08-16 20:01:43 +0530

    Added:UC1

--- Program.cs
+++ Program.cs
@@ -5,6 +5,12 @@
         public static void Main(string[] args)
         {
             Console.WriteLine("Welcome to User Registration Problem");
+
+            //UC1
+            FirstName firstName = new FirstName();
+            Console.WriteLine("Please Enter your first name");
+            var check = firstName.ValidateFirstName(Console.ReadLine());
+            Console.WriteLine(check);
         }
     }
 }
