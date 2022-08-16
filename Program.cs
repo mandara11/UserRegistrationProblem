5a37b7da37eb85acc720be1e315f0934eeb8edb7
mandara11 <mandaram2345@gmail.com>
2022-08-16 19:54:45 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationProblem
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to User Registration Problem");
+        }
+    }
+}
