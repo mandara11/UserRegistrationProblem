fd2c845c4bb940ab715620dfeec68b5e52678d77
mandara11 <mandaram2345@gmail.com>
2022-08-16 20:05:29 +0530

    ValidLastName

---
+++ LastName.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistrationProblem
+{
+    public class LastName
+    {
+        public static string LAST_NAME_REGEX = "^[A-Z]{1}[a-z]{2,}$";
+
+        public bool ValidateFirstName(string lName)
+        {
+            return Regex.IsMatch(lName, LAST_NAME_REGEX);
+        }
+    }
+}
