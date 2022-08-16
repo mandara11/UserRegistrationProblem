ebebbaf71894633fb56c9d1d472a70daf19e529f
mandara11 <mandaram2345@gmail.com>
2022-08-16 22:22:11 +0530

    Minimum*Character

---
+++ Password1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+
+/*
+As a User need to follow pre-defined Password rules.
+Rule1 – minimum 8Characters
+- NOTE – All rules must be passed
+*/
+
+namespace UserRegistrationProblem
+{
+    public class Password1
+    {
+        public static string REGEX_Password1 = "^[A-Z a-z]{8,}$"; //mandaraM, MandaraM
+
+        public bool ValidatePassword1(string password)
+        {
+            return Regex.IsMatch(password, REGEX_Password1);
+        }
+    }
+}
