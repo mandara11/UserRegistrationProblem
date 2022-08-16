bfbfff20890252cf6517b3e0c1411388afa99a75
mandara11 <mandaram2345@gmail.com>
2022-08-16 22:29:15 +0530

    Added:UC6

--- Program.cs
+++ Program.cs
@@ -35,6 +35,12 @@
             Console.WriteLine("Please Enter your password to check");
             var check5 = password1.ValidatePassword1(Console.ReadLine());
             Console.WriteLine(check5);
+
+            //UC6
+            Password2 password2 = new Password2();
+            Console.WriteLine("Please Enter your password to check");
+            var check6 = password2.ValidatePassword2(Console.ReadLine());
+            Console.WriteLine(check6);
         }
     }
 }
