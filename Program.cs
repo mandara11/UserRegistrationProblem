0987aeb826c4b2c8e4280b8da9305cdc2c17cc45
mandara11 <mandaram2345@gmail.com>
2022-08-16 22:39:55 +0530

    Added:Uc8

--- Program.cs
+++ Program.cs
@@ -47,6 +47,12 @@
             Console.WriteLine("Please Enter your password to check");
             var check7 = password3.ValidatePassword3(Console.ReadLine());
             Console.WriteLine(check7);
+
+            //UC8
+            Password4 password4 = new Password4();
+            Console.WriteLine("Please Enter your password to check");
+            var check8 = password4.ValidatePassword4(Console.ReadLine());
+            Console.WriteLine(check8);
         }
     }
 }
